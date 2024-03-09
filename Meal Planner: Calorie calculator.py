c42cdcb19229d0184a69a52726a1d946d4f15422
Alice Lin <146058387+xl2176@users.noreply.github.com>
2024-03-09 15:56:52 -0500

    Add files via upload

--- Meal Planner: Calorie calculator.py
+++ Meal Planner: Calorie calculator.py
@@ -13,36 +13,63 @@ def get_recipe_info(recipe):
     '''
     This function gets data from the recipe
     param: recipe (a list)
-    return: cal_data (a list of dictionary): the data of eac
+    return: data (a list of dictionary): the data of eac
     '''
     #API Call: https://api-ninjas.com/api/nutrition
-    query = get_recipe_info(recipe)
-    api_url = 'https://api.api-ninjas.com/v1/nutrition?query={}'.format(query)
-    response = requests.get(api_url, headers={'X-Api-Key': 'srjEtGD5ts7XRyMuzRZvzA==pKa0qlZ6dXSJfOLx'})
-    cal_data = []
-    if response.status_code == requests.codes.ok:
-        for item in recipe:
-            item = response.text
-            cal_data.append(item)
-        return cal_data
-    else:
-        print("Error:", response.status_code, response.text)
+    data = []
+    for item in recipe:
+        query = item
+        api_url = 'https://api.api-ninjas.com/v1/nutrition?query={}'.format(query)
+        response = requests.get(api_url, headers={'X-Api-Key': 'srjEtGD5ts7XRyMuzRZvzA==pKa0qlZ6dXSJfOLx'})
+        if response.status_code == requests.codes.ok:
+            element = response.json()
+            data.append(element[0])
+        else:
+            print("Error:", response.status_code, response.text)
+    return data
+'''
+def get_quality_info():
+    
+    this function gets quality info from the user
+    param: data
+    return: quality
+    
+    quality = float(input("how many grams?"))
+    return quality
+'''
+def update_data(data):
+    '''
+    this function update the quality data by user input
+    param: data
+    return: data
+    '''
+    for item in data:
+        print(item["name"])
+        quality = float(input("how many grams?"))
+        item['serving_size_g'] = quality
+    return data
 
-
-
-def calculate_calories(recipe):
+def calculate_calories(data):
     '''
     This function calculates the calories of each recipe
-    param: recipe
-    return cal
+    param: data
+    return: Total_cal
     '''
-    total_calories = 0
-    for food in recipe:
-        for item in recipe:
-            if item["name"] == food:
-                total_calories += item["calories"]
-                break
-    return cal
+    total_cal = 0
+    for item in data:
+        total_cal += item.get("calories", 0)
+    return total_cal
+
+def calculate_carbohydrates(data):
+    '''
+    This function calculates the carbohydrates of each recipe
+    param: data
+    return: total_carb
+    '''
+    total_carb = 0
+    for item in data:
+        total_carb += item.get("carbohydrate", 0)
+    return total_carb
 
 def main():
     '''
@@ -51,3 +78,11 @@ def main():
     3. let it run thru the nutrition API, and gets the 'cal': which is an int
     4. add up the total 'cal' for the recipe return the 'daily_cal' -> store that in a database
     '''
+    recipe = ['apple', 'banana', 'orange']
+    data = get_recipe_info(recipe)
+    data = update_data(data)
+    print(data)
+    print(calculate_calories(data))
+    print(calculate_carbohydrates(data))
+    
+main()
